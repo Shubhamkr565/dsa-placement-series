684c427663cf4b533327cc123aeccb0f9415c4b3
Shubham <shubhamkr565@gmail.com>
2026-09-16 00:06:39 +0530

    Target not found

---
+++ src/dsa/BinarySearch/TargetNotFound.java
@@ -0,0 +1,37 @@
+package dsa.BinarySearch;
+
+public class TargetNotFound {
+
+    public static int findTarget(int[] nums, int target){
+        int left = 0;
+        int right = nums.length-1;
+
+        while (left<=right){
+            int mid = left+ (right - left)/2;
+
+            if(nums[mid] == target){
+                return mid;
+            }else if(nums[mid] < target){
+                left = mid+1;
+            }else{
+                right = mid-1;
+            }
+        }
+        return -1;
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = {2, 5, 8, 12, 16, 20, 25};
+        int target = 15;
+
+        int result =  findTarget(arr, target);
+
+        if(result != -1){
+            System.out.println("Target: "+target+" found at index: "+result);
+        }else{
+            System.out.println("Target "+target+" not found");
+        }
+
+    }
+}
