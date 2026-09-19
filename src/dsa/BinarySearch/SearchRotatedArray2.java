1fe86b15371701007cf7406510d695126fc7def6
Shubham <shubhamkr565@gmail.com>
2026-09-19 16:38:47 +0530

    Rotated Array 2

---
+++ src/dsa/BinarySearch/SearchRotatedArray2.java
@@ -0,0 +1,42 @@
+package dsa.BinarySearch;
+
+public class SearchRotatedArray2 {
+
+    public static int search(int[] nums, int target){
+        int left = 0;
+        int right = nums.length-1;
+
+        while (left<=right){
+            int mid = left+(right-left)/2;
+
+            if(nums[mid] == target){
+                return mid;
+            }
+
+            if(nums[left] <= nums[mid]){
+                if(nums[left]<= target && target< nums[mid]){
+                    right = mid-1;
+                }else{
+                    left = mid+1;
+                }
+            }else{
+                if(nums[mid]< target && target <= nums[right]){
+                    left = mid+1;
+                }else{
+                    right = mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = {6, 7, 8, 1, 2, 3, 4, 5};
+        int target = 3;
+
+        int result = search(arr, target);
+
+        System.out.println("Result: "+result);
+    }
+}
