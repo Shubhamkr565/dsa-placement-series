854739ab4ee7d95550bc39d9246fe3d17a740a90
Shubham <shubhamkr565@gmail.com>
2026-09-19 14:07:28 +0530

    Search position

---
+++ src/dsa/BinarySearch/SearchInsertPosition.java
@@ -0,0 +1,34 @@
+package dsa.BinarySearch;
+
+public class SearchInsertPosition {
+
+    public static int findPosition(int[] nums, int target){
+        int left = 0;
+        int right = nums.length-1;
+        while (left<=right){
+            int mid = left+(right-left)/2;
+            if(nums[mid] == target){
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid+1;
+            }else{
+                right = mid-1;
+            }
+        }
+        return left;
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = {1,3,5,6};
+        int target = 5;
+//        int target = 2;
+//        int target = 7;
+
+
+        int result = findPosition(arr, target);
+
+        System.out.println("Result: "+result);
+
+    }
+}
