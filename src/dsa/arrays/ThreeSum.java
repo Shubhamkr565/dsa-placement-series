f734753fb4002508efc9c8e17491be275b927c57
Shubham <shubhamkr565@gmail.com>
2026-09-26 17:40:30 +0530

    Updated

--- src/dsa/arrays/ThreeSum.java
+++ src/dsa/arrays/ThreeSum.java
@@ -3,21 +3,29 @@ package dsa.arrays;
 public class ThreeSum {
 
     public static void main(String[] args) {
-        int[]  arr = {-1,0,1,2-1,4};
 
-        for(int i=0; i<arr.length; i++){
-            for(int j=1; j<arr.length; j++){
-                for(int z=2; z<arr.length; z++){
+        // Input array
+        int[] arr = {-1, 0, 1, 2, -1, 4};
 
-                    if(arr[i]+arr[j]+arr[z]== 0){
-                        System.out.print(arr[i]+","+arr[j]+","+arr[z]+",");
+        // First element
+        for (int i = 0; i < arr.length - 2; i++) {
+
+            // Second element
+            for (int j = i + 1; j < arr.length - 1; j++) {
+
+                // Third element
+                for (int z = j + 1; z < arr.length; z++) {
+
+                    // Check if sum of three elements is 0
+                    if (arr[i] + arr[j] + arr[z] == 0) {
+
+                        // Print the triplet
+                        System.out.println(
+                                arr[i] + ", " + arr[j] + ", " + arr[z]
+                        );
                     }
                 }
             }
         }
-        for(int n: arr){
-            System.out.print(n+",");
-        }
-
     }
 }
